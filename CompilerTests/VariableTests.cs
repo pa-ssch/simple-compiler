37fe1be4bbda922527a03ae4d8f84309be3d01c8
SilasDerProfi <git@schnurr.one>
2020-07-02 14:44:39 +0200

    Addeed test for variable declaration and use

--- CompilerTests/VariableTests.cs
+++ CompilerTests/VariableTests.cs
@@ -8,6 +8,16 @@ namespace CompilerTests
         [TestMethod]
         public void TestAssign()
         {
+            // Plan
+            var input = "x = 9*9;PRINT x;x = 8;y = 2;z = x*y;PRINT x; PRINT z; PRINT y; PRINT x+y+z".ToStream();
+            var output = "81;8;16;2;26;";
+
+            // Do
+            var compiler = new Compiler.CompilerEnvironment();
+            compiler.Compile(input);
+
+            // Check
+            Assert.AreEqual(output, compiler.Output);
         }
     }
 }
